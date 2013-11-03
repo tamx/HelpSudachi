d1b1d28f0a7604577e63c3bde87ad57ec4fd1d1b
Tam <tam.info.math@gmail.com>
2013-11-03 16:19:53 +0900

    fixed some bugs.

--- HelpSudachi/src/jp/cane/helpsudachi/Main.java
+++ HelpSudachi/src/jp/cane/helpsudachi/Main.java
@@ -5,13 +5,22 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.Inet4Address;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import javax.imageio.ImageIO;
 
-public class Main {
-	public static void main(String[] argv) throws IOException {
+public class Main implements Runnable {
+	private Socket socket = null;
+
+	public Main(Socket socket) {
+		this.socket = socket;
+		new Thread(this).start();
+	}
+
+	@Override
+	public void run() {
 		// 取得する画面エリア
 		java.awt.GraphicsEnvironment env = java.awt.GraphicsEnvironment
 				.getLocalGraphicsEnvironment();
@@ -22,24 +31,36 @@ public class Main {
 		int height = displayMode.getHeight();
 		Rectangle screenSize = new Rectangle(0, 0, width, height);
 
-		ServerSocket ssocket = new ServerSocket(8888);
+		try {
+			socket.getOutputStream().write("HTTP/1.0 200\n\n".getBytes());
 
-		while (true) {
+			// java.awt.Robotの生成
+			Robot robot = new Robot();
+			// イメージを所定の場所に出力
+			BufferedImage image = robot.createScreenCapture(screenSize);
+			ImageIO.write(image, "png", socket.getOutputStream());
+
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (AWTException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+		} finally {
 			try {
-				Socket socket = ssocket.accept();
-
-				// java.awt.Robotの生成
-				Robot robot = new Robot();
-				// イメージを所定の場所に出力
-				BufferedImage image = robot.createScreenCapture(screenSize);
-				ImageIO.write(image, "png", socket.getOutputStream());
-
 				socket.close();
 			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (AWTException e) {
-				e.printStackTrace();
 			}
+		}
+	}
+
+	public static void main(String[] argv) throws IOException {
+		ServerSocket ssocket = new ServerSocket(8888, 0,
+				Inet4Address.getByName("0.0.0.0"));
+
+		while (true) {
+			Socket socket = ssocket.accept();
+			new Main(socket);
 		}
 	}
 }
